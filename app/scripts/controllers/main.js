a8716f7de0917300218988c71f59f5634cccff30
Abid Velshi <viable.dish@gmail.com>
2014-02-26 12:01:47 -0500

    refactored code to call API only once at initial page load

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -21,73 +21,67 @@ angular.module('vantageApp')
   getItems();
 
   $scope.partySpend = function () {
-  	$http.jsonp('http://api.nytimes.com/svc/elections/us/v3/finances/2008/president/totals.json?api-key=85c32d59cd9256167606de14f60ebe95:11:20721543&callback=JSON_CALLBACK').success(function (data) {
-      // $scope.chartConfig.series.push = data.results[0].total_receipts;
-  		var demSpend = 0;
-  		var repubSpend= 0;
-  		for (var i = 0; i < data.results.length; i++) {
-				if (data.results[i].party == "D") {
-					demSpend += parseInt(data.results[i].total_receipts);
-				} else {
-					repubSpend += parseInt(data.results[i].total_receipts);
-				}
-  		}
-  		var totalSpend = [['Democrats',demSpend],['Republicans', repubSpend]]
-	    $scope.chartConfig = {
-        options: {
-            chart: {
-                type: 'pie'
-            }
-        },
-        series: [{
-            data: totalSpend,
-            name: 'Party Spend'
-        }],
-        title: {
-        	text: 'US 2008 Campaign Spend by Party'
-    		},
-    		plotOptions: {
-          pie: {
-              allowPointSelect: true,
-              cursor: 'pointer',
+    var demSpend = 0;
+    var repubSpend= 0;
+    for (var i = 0; i < $scope.items.results.length; i++) {
+      if ($scope.items.results[i].party == "D") {
+        demSpend += parseInt($scope.items.results[i].total_receipts);
+      } else {
+        repubSpend += parseInt($scope.items.results[i].total_receipts);
+      }
+    }
+
+
+		var totalSpend = [['Democrats',demSpend],['Republicans', repubSpend]]
+    $scope.chartConfig = {
+      options: {
+          chart: {
+              type: 'pie'
           }
-    		},	
-        loading: false
-	    }
-      console.log($scope.chartConfig.series[0].data.length);
-  	});
+      },
+      series: [{
+          data: totalSpend,
+          name: 'Party Spend'
+      }],
+      title: {
+      	text: 'US 2008 Campaign Spend by Party'
+  		},
+  		plotOptions: {
+        pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+        }
+  		},	
+      loading: false
+    }
   };
 
   $scope.candidateReceipts = function () {
-    var nytUri = 'http://api.nytimes.com/svc/elections/us/v3/finances/2008/president/totals.json?api-key=85c32d59cd9256167606de14f60ebe95:11:20721543&callback=JSON_CALLBACK'
-  	$http.jsonp(nytUri).success(function (data) {
-      // $scope.chartConfig.series.push = data.results[0].total_receipts;
-  		var cd = [];
-  		var names = [];
-  		for (var i = 0; i < data.results.length; i++) {
-  				var receipt = []
-  				receipt = parseInt(data.results[i].total_receipts);
-  				cd.push(receipt);
-  				names.push(data.results[i].candidate_name);
-      }
-	    $scope.chartConfig = {
-        options: {
-            chart: {
-                type: 'bar'
-            }
+		var cd = [];
+		var names = [];
+		for (var i = 0; i < $scope.items.results.length; i++) {
+				var receipt = []
+				receipt = parseInt($scope.items.results[i].total_receipts);
+				cd.push(receipt);
+				names.push($scope.items.results[i].candidate_name);
+    }
+    $scope.chartConfig = {
+      options: {
+          chart: {
+              type: 'bar'
+          }
+      },
+      series: [{
+          data: cd,
+      }],
+      xAxis: {
+    		categories: names, 
+      	},
+        title: {
+            text: 'US 2008 Campaign Total Receipts by Candidate'
         },
-        series: [{
-            data: cd,
-        }],
-        xAxis: {
-      		categories: names, 
-        	},
-          title: {
-              text: 'US 2008 Campaign Total Receipts by Candidate'
-          },
 
-        loading: false
-	    }
-  	});       
+      loading: false
+    }      
   };
 });
