815dc5e2956bb4692060ae40c9807eced7b4bbae
Abid Velshi <viable.dish@gmail.com>
2014-02-27 13:58:55 -0500

    added dynamic filtering of Candidate Receipts data

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('vantageApp')
-  .controller('MainCtrl', function ($scope, $http) {
+  .controller('MainCtrl', function ($scope, $http, $filter) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
@@ -12,76 +12,84 @@ angular.module('vantageApp')
     $scope.chartData = [];
     $scope.chartConfig = {};
 
-  var getItems = function() {
-    $http.jsonp('http://api.nytimes.com/svc/elections/us/v3/finances/2008/president/totals.json?api-key=85c32d59cd9256167606de14f60ebe95:11:20721543&callback=JSON_CALLBACK').success(function (data) {
-      $scope.items = data;
-    });
-  };
+    var getItems = function() {
+      $http.jsonp('http://api.nytimes.com/svc/elections/us/v3/finances/2008/president/totals.json?api-key=85c32d59cd9256167606de14f60ebe95:11:20721543&callback=JSON_CALLBACK').success(function (data) {
+        $scope.items = data;
+      });
+    };
 
-  getItems();
+    getItems();
 
-  $scope.partySpend = function () {
-    var demSpend = 0;
-    var repubSpend= 0;
-    for (var i = 0; i < $scope.items.results.length; i++) {
-      if ($scope.items.results[i].party == "D") {
-        demSpend += parseInt($scope.items.results[i].total_receipts);
-      } else {
-        repubSpend += parseInt($scope.items.results[i].total_receipts);
-      }
-    }
+    // watch the expression, and update the UI on change.
+    $scope.$watch('candidateFilter', function () {
+      $scope.candidateReceipts($scope.filteredItems);
+    }, true);
 
+    $scope.partySpend = function () {
+      var demSpend = 0;
+      var repubSpend= 0;
 
-		var totalSpend = [['Democrats',demSpend],['Republicans', repubSpend]]
-    $scope.chartConfig = {
-      options: {
-          chart: {
-              type: 'pie'
-          }
-      },
-      series: [{
-          data: totalSpend,
-          name: 'Party Spend'
-      }],
-      title: {
-      	text: 'US 2008 Campaign Spend by Party'
-  		},
-  		plotOptions: {
-        pie: {
-            allowPointSelect: true,
-            cursor: 'pointer',
+      for (var i = 0; i < $scope.items.results.length; i++) {
+        if ($scope.items.results[i].party == "D") {
+          demSpend += parseInt($scope.items.results[i].total_receipts);
+        } else {
+          repubSpend += parseInt($scope.items.results[i].total_receipts);
         }
-  		},	
-      loading: false
-    }
-  };
+      }
 
-  $scope.candidateReceipts = function () {
-		var cd = [];
-		var names = [];
-		for (var i = 0; i < $scope.items.results.length; i++) {
-				var receipt = []
-				receipt = parseInt($scope.items.results[i].total_receipts);
-				cd.push(receipt);
-				names.push($scope.items.results[i].candidate_name);
-    }
-    $scope.chartConfig = {
-      options: {
-          chart: {
-              type: 'bar'
-          }
-      },
-      series: [{
-          data: cd,
-      }],
-      xAxis: {
-    		categories: names, 
-      	},
-        title: {
-            text: 'US 2008 Campaign Total Receipts by Candidate'
+  		var totalSpend = [['Democrats',demSpend],['Republicans', repubSpend]]
+      
+      $scope.chartConfig = {
+        options: {
+            chart: {
+                type: 'pie'
+            }
         },
+        series: [{
+            data: totalSpend,
+            name: 'Party Spend'
+        }],
+        title: {
+        	text: 'US 2008 Campaign Spend by Party'
+    		},
+    		plotOptions: {
+          pie: {
+              allowPointSelect: true,
+              cursor: 'pointer',
+          }
+    		},	
+        loading: false
+      }
+    };
 
-      loading: false
-    }      
-  };
+    $scope.candidateReceipts = function (data) {
+  		var cd = [];
+  		var names = [];
+  		
+      for (var i = 0; i < data.length; i++) {
+  				var receipt = []
+  				receipt = parseInt(data[i].total_receipts);
+  				cd.push(receipt);
+  				names.push(data[i].candidate_name);
+      }
+
+      $scope.chartConfig = {
+        options: {
+            chart: {
+                type: 'bar'
+            }
+        },
+        series: [{
+            data: cd,
+        }],
+        xAxis: {
+      		categories: names, 
+        	},
+          title: {
+              text: 'US 2008 Campaign Total Receipts by Candidate'
+          },
+
+        loading: false
+      }  
+    };
 });
